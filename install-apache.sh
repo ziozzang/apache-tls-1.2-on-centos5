a58e318c2ead1c27bce5631818c59354a4645fc7
root <root@localhost.localdomain>
2018-07-15 21:48:18 +0400

    [hackers.mu][add latest openssl and apache for TLS 1.3]

--- install-apache.sh
+++ install-apache.sh
@@ -3,9 +3,11 @@ CWD=`pwd`
 WORKING_PATH="/opt/src"
 # - both 1.0.2 and 1.0.1 works well...
 #OPENSSL_VERSION="1.0.2d"
-OPENSSL_VERSION="1.0.1p"
+OPENSSL_VERSION="1.1.1-pre8"
 # - Tested apache 2.2.31 (current, newest version)
-APACHE_VERSION="2.2.31"
+APACHE_APR_VERSION="1.6.3"
+APACHE_APR_UTIL_VERSION="1.6.1"
+APACHE_VERSION="2.4.34"
 # - prefix of openssl & apache...
 PREFIX="/opt"
 # - Target apache path. this will be /opt/apache
@@ -30,21 +32,30 @@ cd openssl-${OPENSSL_VERSION}
 # - download and install Apache 2.2.X
 cd ${WORKING_PATH}
 wget http://apache.mirror.cdnetworks.com//httpd/httpd-${APACHE_VERSION}.tar.gz
+wget http://www-us.apache.org/dist/apr/apr-${APACHE_APR_VERSION}.tar.gz
+wget http://www-us.apache.org/dist/apr/apr-util-${APACHE_APR_UTIL_VERSION}.tar.gz
 tar -xzvf httpd-${APACHE_VERSION}.tar.gz
-cd httpd-${APACHE_VERSION}
+mkdir -p ${WORKING_PATH}/httpd-${APACHE_VERSION}/srclib/apri/
+mkdir -p ${WORKING_PATH}/httpd-${APACHE_VERSION}/srclib/apr-util/
+tar -xzvf apr-${APACHE_APR_VERSION}.tar.gz --strip-components 1 -C ${WORKING_PATH}/httpd-${APACHE_VERSION}/srclib/apr
+
+tar -xzvf apr-util-${APACHE_APR_UTIL_VERSION}.tar.gz --strip-components 1 -C ${WORKING_PATH}/httpd-${APACHE_VERSION}/srclib/apr-util
 
 # -- this makes link of openssl header accessable. (Very important)
-ln -s ${PREFIX}/include ${PREFIX}/openssl/include
+#ln -s ${PREFIX}/include ${PREFIX}/openssl/include
+
+cd ${WORKING_PATH}/httpd-${APACHE_VERSION}/
 
 ./configure \
   --enable-so --enable-ssl \
   --with-ssl=${PREFIX}/openssl \
-  --prefix=${PREFIX}${APACHE_PATH}
+  --prefix=${PREFIX}${APACHE_PATH} \
+  --with-included-apr
 
 # -- this makes link of openssl library accessable. (Very important)
 #    this code !!!!!must run after ./configuration !!!!! (veeeeery important) 
 #    this is tricky code. but this works.
-ln -s ${PREFIX}/lib ${PREFIX}/openssl/lib
+#ln -s ${PREFIX}/lib ${PREFIX}/openssl/lib
 
 # -- build apache.
 make install
